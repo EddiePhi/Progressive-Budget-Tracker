0742fb7899cdc7a6bfa0d9915d90098a058851fc
Eddie Phimansone <ephimansone@gmail.com>
2021-02-07 12:48:51 -0500

    added db.js reference

--- client/service-worker.js
+++ client/service-worker.js
@@ -5,11 +5,13 @@ const FILES_TO_CACHE = [
     '/assets/js/index.js',
     '/assets/icons/icon-192x192.png',
     '/assets/icons/icon-512x512.png',
+    '/manifest.webmanifest',
+    'webpack.config.js',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
   ];
   
-  const PRECACHE = 'precache-v1';
+  const PRECACHE = 'budget-cache-v1';
   const RUNTIME = 'runtime';
   
   self.addEventListener('install', (event) => {
