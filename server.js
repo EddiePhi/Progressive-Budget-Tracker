f6483772a77a27bc3dbbf9b3eb1b7117f78f6d41
Eddie Phimansone <ephimansone@gmail.com>
2021-02-06 22:14:19 -0500

    index.html route and style.css working

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("client"));
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
