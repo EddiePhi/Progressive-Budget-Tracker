12288ad85166a55a49a6d97c6640784180ac0cec
Eddie Phimansone <ephimansone@gmail.com>
2021-02-09 18:05:55 -0500

    able to repopulate page offline

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,12 +1,12 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/assets/css/style.css',
-    '/assets/js/index.js',
+    'assets/js/index.js',
+    'assets/js/db.js',
+    '/assets/css/styles.css',
     '/assets/icons/icon-192x192.png',
     '/assets/icons/icon-512x512.png',
     '/manifest.webmanifest',
-    'webpack.config.js',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
   ];
@@ -18,8 +18,10 @@ const FILES_TO_CACHE = [
     event.waitUntil(
       caches
         .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+        .then((cache) => {
+          console.log(cache)
+          cache.addAll(FILES_TO_CACHE)
+        })
     );
   });
   
